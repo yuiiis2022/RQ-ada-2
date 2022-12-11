0bce9c2bb3da75a19ef9f8b3520718e336466e72
yuiiis2022 <106508994+yuiiis2022@users.noreply.github.com>
2022-12-11 03:57:35 +0000

    Add files via upload

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'c5a0fa34-8ec9-4b02-9e01-4e3a9e30e206'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
